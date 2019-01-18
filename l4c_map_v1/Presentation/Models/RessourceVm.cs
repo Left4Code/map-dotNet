bae6c9cbc2f7751c840582795d421d0d27433b7d
badis <mohamedbadis.maalej@esprit.tn>
2019-01-18 18:19:13 +0100

    commit for commerciale

---
+++ l4c_map_v1/Presentation/Models/RessourceVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.Models
+{
+    public class RessourceVm
+    {
+        public RessourceVm()
+        {
+
+        }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public float cost { get; set; }
+        public IEnumerable<SkillVM> skills { get; set; }
+        public IEnumerable<MandateVM> listemandate { get; set; }
+    }
+}
---
+++ l4c_map_v1/Presentation/Models/SkillVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.Models
+{
+    public class SkillVM
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
